1a788b6e84821396d7f1261714a96def1a6f0ecd
igorfals <igorfeals@gmail.com>
2023-03-21 18:25:59 -0300

    feat: responsive mobile

---
+++ assets/profission.js
@@ -0,0 +1,24 @@
+const logoMain = document.querySelector('.service__logo__main');
+const divs = logoMain.querySelectorAll('div');
+divs.forEach(element => {
+    element.addEventListener('click', (event) => {
+        divs.forEach(async (divoriginal) => {
+            const alterarItem = divoriginal.querySelector('div')
+            if (alterarItem !== null) {
+                if (alterarItem.classList.contains('service__profission')) {
+                    alterarItem.classList.remove('service__profission')
+                }
+                alterarItem.classList.add('rotated')
+                divoriginal.style.width = '20%'
+            }
+        });
+        const alterar = element.querySelector('div')
+        if (alterar !== null) {
+            if (alterar.classList.contains('rotated')) {
+                alterar.classList.remove('rotated')
+            }
+            alterar.classList.add('service__profission')
+            element.style.width = '40%'
+        }
+    });
+});

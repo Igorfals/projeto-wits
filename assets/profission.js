370b27d231cdc3acf060c953d04ccefc92835988
igorfals <igorfeals@gmail.com>
2023-03-22 17:44:37 -0300

    feat: responsive

--- assets/profission.js
+++ assets/profission.js
@@ -5,22 +5,27 @@ divs.forEach(element => {
         divs.forEach(async (divoriginal) => {
             const alterarItem = divoriginal.querySelector('div')
             if (alterarItem !== null) {
-                if (alterarItem.classList.contains('service__profission')) {
-                    alterarItem.classList.remove('service__profission')
-                }
-                alterarItem.classList.add('rotated')
-                alterarItem.classList.remove('not-rotated')
                 divoriginal.style.width = '20%'
+                setTimeout(() => {
+                    if (alterarItem.classList.contains('service__profission')) {
+                        alterarItem.classList.remove('service__profission')
+                    }
+                    alterarItem.classList.add('rotated')
+                    alterarItem.classList.remove('not-rotated')
+                }, 100)
+
             }
         });
         const alterar = element.querySelector('div')
         if (alterar !== null) {
-            if (alterar.classList.contains('rotated')) {
-                alterar.classList.remove('rotated')
-                alterar.classList.add('not-rotated')
-            }
-            alterar.classList.add('service__profission')
             element.style.width = '40%'
+            setTimeout(() => {
+                if (alterar.classList.contains('rotated')) {
+                    alterar.classList.remove('rotated')
+                    alterar.classList.add('not-rotated')
+                }
+                alterar.classList.add('service__profission')
+            }, 100)
         }
     });
 });

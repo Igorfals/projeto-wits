b1094b05c933d275a61ba1f97ea3f36fb5755093
igorfals <igorfeals@gmail.com>
2023-03-22 11:19:13 -0300

    feat: criando carousel e adicionando hover em btn

--- assets/slide.js
+++ assets/slide.js
@@ -12,3 +12,17 @@ prevButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
     slide.scrollLeft -= slideWidth;
 });
+
+const images = document.querySelectorAll('#slide img');
+function rodar() {
+    [images[0].src, images[1].src, images[2].src, images[3].src, images[4].src] = [images[1].src, images[2].src, images[3].src, images[4].src, images[0].src]
+    images[2].classList.remove('active');
+    images[2].classList.add('active2');
+    setTimeout(() => {
+        images[2].classList.remove('active2');
+        images[2].classList.add('active');
+    }, 400)
+}
+setInterval(() => {
+    rodar()
+}, 6000);
